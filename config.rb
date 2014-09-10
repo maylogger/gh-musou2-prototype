f21721c38361f8129b412b51f3e80806e0c3953d
Tou Neko <touneko@gmail.com>
2014-09-10 20:59:28 +0800

    啟用 depoly config

--- config.rb
+++ config.rb
@@ -27,11 +27,11 @@ line_comments = false
 # sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
 preferred_syntax = :scss
 
-# the_hold_options = {
-#   :login => "evenwu", # 你的帳號
-#   :token => "229f2b40d4f17f98f142318250a75f20", # 你的密碼
-#   :project => "musou2", # 專案名稱, 會變成 http://your-project-name.evenwu.the-hold.handlino.com/
-#   #:project_site_password => "musou2design", # 密碼，這一行拿掉的話就沒有密碼
-#   :host => "http://the-hold.handlino.com/",
-#   :cname => "musou2.evendesign.tw"
-# }
+the_hold_options = {
+  :login => "evenwu", # 你的帳號
+  :token => "229f2b40d4f17f98f142318250a75f20", # 你的密碼
+  :project => "musou2", # 專案名稱, 會變成 http://your-project-name.evenwu.the-hold.handlino.com/
+  # :project_site_password => "musou2design", # 密碼，這一行拿掉的話就沒有密碼
+  :host => "http://the-hold.handlino.com/",
+  # :cname => "musou2.evendesign.tw"
+}
