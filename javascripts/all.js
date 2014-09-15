a94e2581c7d67f585007aac792f72cc243224a9a
Tou Neko <touneko@gmail.com>
2014-09-15 19:07:57 +0800

    新增 collapse 相關狀態 js

--- javascripts/all.js
+++ javascripts/all.js
@@ -6,3 +6,19 @@ $('.menu-btn').on("click", function(){
 $('.news-comment-btn button').on("click", function(){
   $(this).parent().next().next().addClass('active');
 });
+
+$('.topic-detail-toggle').on("click", function(){
+  $(this).toggleClass('is-open');
+});
+
+$('.topic-detail').on("hide.bs.collapse", function(){
+  $(this).prev('.topic-header').children('.topic-detail-toggle').removeClass('is-open');
+});
+
+$('.month-title').on("click", function(){
+  $(this).toggleClass('is-open');
+});
+
+$('.month-post-list').on("hide.bs.collapse", function(){
+  $(this).prev('.month-title').removeClass('is-open');
+});
