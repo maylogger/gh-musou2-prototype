562ac679875d21108de21bca7887c1dbc1af0ade
Tou Neko <touneko@gmail.com>
2014-10-02 20:28:58 +0800

    新增網站互動項目 JS

--- javascripts/all.js
+++ javascripts/all.js
@@ -3,8 +3,10 @@ $('.menu-btn').on("click", function(){
   $('body').toggleClass('open-menu');
 });
 
-$('.news-comment-btn button').on("click", function(){
-  $(this).parent().next().next().addClass('active');
+$('.comment-button').on("click", function(){
+  $(this).next('.comment-input').addClass('is-open');
+  $(this).parent('.comment-form').addClass('is-open');
+  $(this).remove();
 });
 
 $('.topic-detail-toggle').on("click", function(){
@@ -22,3 +24,59 @@ $('.month-title').on("click", function(){
 $('.month-post-list').on("hide.bs.collapse", function(){
   $(this).prev('.month-title').removeClass('is-open');
 });
+
+$('.catalog').hcSticky({
+  top: 24,
+  responsive: true,
+  stickTo: '.main > .container',
+  offResolutions: -900
+});
+
+$('.go-anchor').on('click',function (e) {
+    e.preventDefault();
+
+    var target = this.hash,
+    $target = $(target);
+
+    $('html, body').stop().animate({
+        'scrollTop': $target.offset().top
+    }, 700, 'swing', function () {
+        window.location.hash = target;
+    });
+});
+
+$('.dictionary-menu-toggle').on("click", function(){
+  $('.dictionary-menu').toggleClass('is-open');
+});
+
+$('.writer-intro-button').on("click", function(){
+  $('.writer-intro').addClass('is-open');
+});
+
+$('.writer-intro .close').on("click", function(){
+  $('.writer-intro').removeClass('is-open');
+});
+
+var videoContentSize =function () {
+  videoContentHeight = $(window).height() - 196;
+  $('.is-scroll').css('height', videoContentHeight);
+  if ( $(window).width() < 900 ) {
+    $('.video-content').removeClass('is-scroll');
+    $('.video-content').css('height', 'auto');
+  }
+  if ( $(window).width() > 900 ) {
+    $('.video-content').addClass('is-scroll');
+  }
+}
+videoContentSize();
+
+$(window).resize(function(){
+  videoContentSize();
+});
+
+$('.search-bar-toggle').on("click", function(){
+  $(this).toggleClass('is-open');
+  $('.search-bar').toggleClass('is-open');
+});
+
+$('.article p').macho();
